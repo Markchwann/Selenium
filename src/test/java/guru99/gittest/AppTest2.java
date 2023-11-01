cca19003af7ccc0cbfe4a45014f8b21e879113fb
Anisa Isa <50681695+Markchwann@users.noreply.github.com>
2023-11-01 21:49:07 +0700

    Update and rename AppTest.java to AppTest2.java

---
+++ src/test/java/guru99/gittest/AppTest2.java
@@ -0,0 +1,12 @@
+// src/test/java/com/example/MyTest.java
+
+package guru99.gittest;
+
+import org.testng.annotations.Test;
+
+public class AppTest2 {
+    @Test
+    public void testExample() {
+        System.out.println("This is a sample test.");
+    }
+}
